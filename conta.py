69b1526ae861b2c3c4b0d1f22e69fabc3a231258
pietro rael <pvpp859@gmail.com>
2026-05-06 21:30:33 -0300

    chekpoint 3

---
+++ conta.py
@@ -0,0 +1,11 @@
+class Conta:
+    def __init__(self,numero,cliente):
+        self.numero = numero
+        self.cliente = cliente
+        self._saldo = 0
+
+    def sacar():
+        pass
+
+    def depositar():
+        pass    
